5943418b9e0895d79767aee368a57dfe9ca31b4d
DinoCzar <ryanb_booking@yahoo.com>
2023-10-19 00:30:25 -0700

    Update files

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ var __webpack_exports__ = {};
 /*!**********************!*\
   !*** ./src/index.js ***!
   \**********************/
-
+console.log('battle')
 /******/ })()
 ;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBLHFCIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vYmF0dGxlc2hpcC8uL3NyYy9pbmRleC5qcyJdLCJzb3VyY2VzQ29udGVudCI6WyJjb25zb2xlLmxvZygnYmF0dGxlJykiXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
