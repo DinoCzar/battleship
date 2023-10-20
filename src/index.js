5b84d8af8b7c6d1cc18830aec162ee1930b5fbd5
DinoCzar <ryanb_booking@yahoo.com>
2023-10-19 20:51:57 -0700

    Add createShip function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,36 @@
-console.log('battle eee')
+function createShip(name, length, hits, sunk) {
+    // Create an empty object
+    const ship = {};
+  
+    // Assign properties to the object
+  
+  ship.name = name;
+  ship.length = length;
+  ship.hits = hits;
+  ship.sunk = sunk;
+  
+    // Define a method for the object
+    ship.hit = function() {
+       ship.hits += 1;
+    };
+  
+  ship.isSunk = function() {
+      if (ship.hits === ship.length) {
+        ship.sunk = 'yes';
+      }
+    };
+  
+    // Return the object
+    return ship;
+  }
+  
+  const aircraftCarrier = createShip('Aircraft Carrier', 5, 0, 'No');
+  
+  console.log(aircraftCarrier.name, aircraftCarrier.length, aircraftCarrier.hits, aircraftCarrier.sunk);
+  
+  aircraftCarrier.hit();
+  // adds hit to ship 
+  
+  aircraftCarrier.isSunk();
+  // checks if ship is sunk and returns sunk if it is 
+  
