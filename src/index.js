93aaf7b6d11b410d472770ccc3312037991055c6
DinoCzar <ryanb_booking@yahoo.com>
2023-10-19 21:51:09 -0700

    Edit syntax to pass jest testing

--- src/index.js
+++ src/index.js
@@ -1,31 +1,29 @@
 function createShip(name, length, hits, sunk) {
-    // Create an empty object
-    const ship = {};
-  
-    // Assign properties to the object
-  
-  ship.name = name;
-  ship.length = length;
-  ship.hits = hits;
-  ship.sunk = sunk;
-  
-    // Define a method for the object
-    ship.hit = function() {
-       ship.hits += 1;
-    };
-  
-  ship.isSunk = function() {
-      if (ship.hits === ship.length) {
-        ship.sunk = 'yes';
-      }
-    };
-  
-    // Return the object
-    return ship;
-  }
-  
+	// Create an empty object
+	const ship = {};
 
-  module.exports = {
+	// Assign properties to the object
+
+	ship.name = name;
+	ship.length = length;
+	ship.hits = hits;
+	ship.sunk = sunk;
+
+	// Define a method for the object
+	ship.hit = function () {
+		ship.hits += 1;
+	};
+
+	ship.isSunk = function () {
+		if (ship.hits === ship.length) {
+			ship.sunk = 'Yes';
+		}
+	};
+
+	// Return the object
+	return ship;
+}
+
+module.exports = {
 	createShip: createShip,
 };
-  
--- src/index.test.js
+++ src/index.test.js
@@ -3,3 +3,35 @@ const {createShip} = require('./index');
 test('should have zero hits', () => {
     expect((createShip('Aircraft Carrier', 5, 0, 'No').hits)).toBe(0);
   });
+
+  describe('Ship', () => {
+    it('should have zero hits', () => {
+      const aircraftCarrier = createShip('Aircraft Carrier', 5, 0, 'No');
+      expect(aircraftCarrier.hits).toEqual(0);
+    });
+  
+    it('should have a length of 5', () => {
+        const aircraftCarrier = createShip('Aircraft Carrier', 5, 0, 'No');
+      expect(aircraftCarrier.length).toEqual(5);
+    });
+  
+  it('should increase hits by 1 when ship is hit', () => {
+    const aircraftCarrier = createShip('Aircraft Carrier', 5, 0, 'No');
+      const initialHits = aircraftCarrier.hits;
+      aircraftCarrier.hit();
+      expect(aircraftCarrier.hits).toEqual(initialHits + 1);
+    });
+  
+  it('should sink after 5 hits', () => {
+    const aircraftCarrier = createShip('Aircraft Carrier', 5, 0, 'No');
+    aircraftCarrier.hit();
+    aircraftCarrier.hit();
+    aircraftCarrier.hit();
+    aircraftCarrier.hit();
+    aircraftCarrier.hit();
+    aircraftCarrier.isSunk();
+      expect(aircraftCarrier.sunk).toEqual('Yes');
+    });
+  
+  });
+  
